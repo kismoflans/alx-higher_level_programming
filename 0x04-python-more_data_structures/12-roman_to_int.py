4e21e8b1fff6e532fe5e7d43af470db74904ee02
Amos <amoslimo50@yahoo.com>
2024-08-18 18:58:55 +0300

    Updated 12-roman

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,29 +1,30 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    """Converts a roman numeral to an integer."""
-    if (not isinstance(roman_string, str) or
-            roman_string is None):
-        return (0)
+    """Converts a Roman numeral to an integer."""
+    if not isinstance(roman_string, str):
+        return 0
 
     roman_dict = {
-            "I": 1,
-            "V": 5,
-            "X": 10,
-            "L": 50,
-            "C": 100,
-            "D": 500,
-            "M": 1000
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
     }
-    num = 0
 
-    for i in range(len(roman_string)):
-        if roman_dict.get(roman_string[i], 0) == 0:
-            return (0)
+    total = 0
+    prev_value = 0
 
-        if (i != (len(roman_string) - 1) and
-                roman_dict[roman_string[i]] < roman_dict[roman_string[i + 1]]):
-                num += roman_dict[roman_string[i]] * -1
-
+    for char in roman_string:
+        value = roman_dict.get(char, 0)
+        if value == 0:
+            return 0
+        if value > prev_value:
+            total += value - 2 * prev_value  # Corrects the over-addition
         else:
-            num += roman_dict[roman_string[i]]
-    return (num)
+            total += value
+        prev_value = value
+
+    return total
