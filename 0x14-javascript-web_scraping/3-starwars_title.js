974b5c30b7f772ee1d9c96f1927385f0e4c2220a
Amos Kiplimo <168921899+kismoflans@users.noreply.github.com>
2024-08-19 08:32:57 +0300

    Update 3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,13 @@
 #!/usr/bin/node
 const request = require('request');
-let url = 'http://swapi.co/api/films/' + process.argv[2];
+
+const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+
 request(url, function (error, response, body) {
-  console.log(error || JSON.parse(body).title);
+  if (!error && response.statusCode === 200) {
+    const data = JSON.parse(body);
+    console.log(data.title);
+  } else {
+    console.error('Error:', error);
+  }
 });
